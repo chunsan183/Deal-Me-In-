e6e03015ff47e00020447440645823077ebc32af
studentadmin <studentadmin@192.168.1.119>
2020-03-16 21:41:55 +0800

    Initial commit

---
+++ src/deal/me/in/DealMeIn.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deal.me.in;
+
+/**
+ *
+ * @author studentadmin
+ */
+public class DealMeIn {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Create a Java project called "Deal Me In!" and share it with me using GitHub.
+        //Remember in a standard deck of playing cards you have four sets of 2, 3, 4, 5, 6, 7, 8, 9,
+        //10, Jack Queen, King, and Ace. Within each set of 4 there are 4 suits: Clubs, Spades,
+        //Hearts, and Diamonds. Create a Card class (with appropriate constructor, setter,
+        //and getter methods) and then create a method to create a stack of 52 cards. To test
+        //your deck, pop each card and print its contents.
+        
+        //Following this, write code that will prompt the user for two numbers: one for the number of
+        //players (x) (max 4 players), two for the number of cards each player will receive (y).
+        //Create x amount of stacks and push y amount of cards in each that are popped from your
+        //deck of 52 cards. There should be no repeated cards and the deal should not happen if
+        //more than 52 cards need to be dealt.
+        
+    }
+    
+}
